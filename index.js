9085b8124e08380cf4410412c4403d815730a9bb
minamedia <mina.m3dia@gmail.com>
2021-07-18 09:09:43 +0100

    html-setup

---
+++ index.js
@@ -0,0 +1,37 @@
+
+// 1. Create two variables, firstCard and secondCard. 
+// Set their values to a random number between 2-11 // 
+// 2. Create a variable, sum, and set it to the sum of the two cards 
+
+
+
+
+
+let age = 2;
+
+if (age < 21) {
+    console.log("You are not old enough to join")
+} else {
+    console.log("Welcome to our club, good Luck!")
+}
+
+
+
+
+let firstCard = 11
+let secondCard = 10
+let sum = firstCard + secondCard
+
+
+
+if (sum <= 20) {
+    console.log("Do you want to draw a new card? 🙂")
+} else if (sum === 21) {
+    console.log("Wohoo! You've got Blackjack! 🥳")
+} else {
+    console.log("You're out of the game! 😭")
+}
+
+
+
+
